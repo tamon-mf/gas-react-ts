38d0cf2d86ec0917020f7a34aa9a51e20b02f678
tamon-mf <okamoto.tamon@moneyforward.co.jp>
2025-12-10 11:26:02 +0900

    Add @tanstack/react-query for data fetching; implement useDevices hook and update App component to utilize device selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import "./App.css";
 import { Button, MultipleSelectBox } from "@moneyforward/mfui-components";
+import { useDevices } from "./hooks/useDevices";
 
 function App() {
 	const [count, setCount] = useState(0);
+
+	const { data, isLoading } = useDevices();
 
 	return (
 		<>
@@ -19,20 +22,12 @@ function App() {
 			<div className="flex flex-col gap-2">
 				<Button priority="primary">MFUI Button</Button>
 				<MultipleSelectBox
-					options={[
-						{
-							label: "Option 1",
-							value: "option1",
-						},
-						{
-							label: "Option 2",
-							value: "option2",
-						},
-						{
-							label: "Option 3",
-							value: "option3",
-						},
-					]}
+					placeholder="Select devices"
+					options={data?.map((device) => ({
+						label: device.name,
+						value: device.id,
+					}))}
+					loading={isLoading}
 				/>
 			</div>
 		</>
